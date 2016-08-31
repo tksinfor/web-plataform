68cd31e4f15c4f887b3af1db71e88e03c1d8bf5f
Takashi <tksinfor@terra.com.br>
2016-08-31 08:46:02 -0300

    para google cloud

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
+import { Angular2QuickstartPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('angular2-quickstart App', function() {
+  let page: Angular2QuickstartPage;
 
-  let expectedMsg = 'My First Angular 2 App';
-
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new Angular2QuickstartPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
